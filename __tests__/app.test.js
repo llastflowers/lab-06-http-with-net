57a225a3d8c12d8e6c38f326d4195fd82ae90d91
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-09 17:04:20 -0800

    fixed parseRequest regex

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('creates a GET route that responds with hi');
+  
 });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./lib/app');
 
 app.listen(7890, () => {
-    console.log('Application started!');
+  console.log('Application started!');
 });
