baa2203a870a4aff163e138d303c33af84bd7e70
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-09 17:41:17 -0800

    one route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-  it('creates a GET route that responds with hi');
-  
+  it('creates a GET route at the path / that responds with hi', () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.text).toEqual('hi');
+        expect(response.status).toEqual(200);
+      });
+  });
 });
